7bbf3bd8c984c385a6c7e0c5a8be3f0d183637a4
Ri4an <redacai.38@gmail.com>
2021-07-27 14:09:01 +0600

    initial project

---
+++ pages/posts/index.tsx
@@ -0,0 +1,6 @@
+import { Template } from '../../components/Template'
+
+export default () => {
+    // const posts = useSelector(state => state.posts)
+    return <Template title='Posts'>Posts</Template>
+}
