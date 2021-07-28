29df36472f818d843ea2728a544bdfaa10cfb8ad
Ri4an <redacai.38@gmail.com>
2021-07-28 22:30:29 +0600

    end thunk

--- models/postsReducer.ts
+++ models/postsReducer.ts
@@ -1,16 +1,20 @@
 import { createSlice, Dispatch } from '@reduxjs/toolkit'
-import { changePost, createPost, getPosts, deletePost, addComment } from './api/posts'
+import { getPost, getPosts } from './api/posts'
 import type { Post } from './types/Post'
 
 const slice = createSlice({
     name: 'posts',
     initialState: {
         posts: [] as Post[],
-        loading: false,
+        currentPost: {} as Post,
+        loading: true,
     },
     reducers: {
         setPosts(s, { payload }: { payload: Post[] }) {
             s.posts = payload
+        },
+        setPost(s, { payload }: { payload: Post }) {
+            s.currentPost = payload
         },
         setLoading(s, { payload }: { payload: boolean }) {
             s.loading = payload
@@ -23,35 +27,11 @@ export const getPostsAsync = () => async (dispatch: Dispatch<any>) => {
     dispatch(setPosts(await getPosts()))
     dispatch(setLoading(false))
 }
-
-export const addPostAsync =
-    (title: string, body: string) => async (dispatch: Dispatch<any>) => {
-        dispatch(setLoading(true))
-        await createPost(title, body)
-        dispatch(setPosts(await getPosts()))
-        dispatch(setLoading(false))
-    }
-export const changePostAsync =
-    (postId: number, title: string, body: string) =>
-    async (dispatch: Dispatch<any>) => {
-        dispatch(setLoading(true))
-        await changePost(postId, title, body)
-        dispatch(setPosts(await getPosts()))
-        dispatch(setLoading(false))
-    }
-export const deletePostAsync = (postId: number) => async (dispatch: Dispatch<any>) => {
+export const getPostAsync = (postId: number) => async (dispatch: Dispatch<any>) => {
     dispatch(setLoading(true))
-    await deletePost(postId)
-    dispatch(setPosts(await getPosts()))
+    dispatch(setPost(await getPost(postId)))
     dispatch(setLoading(false))
 }
-export const addCommentAsync =
-    (postId: number, body: string) => async (dispatch: Dispatch<any>) => {
-        dispatch(setLoading(true))
-        await addComment(postId, body)
-        dispatch(setPosts(await getPosts()))
-        dispatch(setLoading(false))
-    }
 
-export const { setPosts, setLoading } = slice.actions
+export const { setPosts, setPost, setLoading } = slice.actions
 export default slice.reducer
